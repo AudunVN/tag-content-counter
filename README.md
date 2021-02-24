4c5792d778bf311705a2856027b363acf0f98632
Space <space@471.no>
2021-02-24 15:48:13 +0100

    Expanded documentation, improved web page layout and responsiveness

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
 # tag-content-counter
 Counts occurences of tag contents in an XML file. Useful for seeing how often a place name or similar occurs in a GML file. If a zip file is specified as input, the script will try parse the largest file in the archive.
 
+Output files are stored in `/output` with the same file name as the input file, but with `.json` or `.html` added at the end. The HTML file only includes the first 1000 rows, the rest are loaded by DataTables from the `.json` file if JavaScript is enabled.
+
 ## Usage
-node counter.js "tagname without brackets" "input file" 
-node counter.js "gn:text" "input/inspire-gn.gml"
-node counter.js "gn:text" "input/inspire-gn.zip"
-node counter.js "app:komplettskrivemåte" "Basisdata_0000_Norge_25833_Stedsnavn_GML.zip"
-node counter.js "app:langnavn" "Basisdata_0000_Norge_25833_StedsnavnKomplettSSR_GML.zip"
+node counter.js "tagname without brackets" "input file" "optional title" "optional description"
+
+### Examples
+```bash
+node counter.js "gn:text" "input/inspire-gn.gml" 
+```
+Outputs `.json` and `.html` files with tag content counts for `inspire-gn.gml`. The title will be generated from the tag and input file name.
+```bash
+node counter.js "gn:text" "input/inspire-gn.zip" "Place Name Frequency in Norway"
+```
+Outputs `.json` and `.html` files with tag content counts for `inspire-gn.zip`. The title will be set to `Place Name Frequency in Norway`.
+
+```bash
+node counter.js "gn:text" "input/inspire-gn.zip" "Place Name Frequency in Norway" "Shows the amount of times place names are used in Norway's INSPIRE place name data set. Data from Kartverket."
+```
+Outputs `.json` and `.html` files with tag content counts for `inspire-gn.zip`. A title and description will be added.
+
+## Current data
+```bash
+node counter.js "app:langnavn" "Basisdata_0000_Norge_25833_StedsnavnKomplettSSR_GML.zip" "Place Name Frequency in Norway" "Shows the amount of times all official place names in Norway are used. Data from Kartverket."
+```
+Complete place name frequencies for Norway.
+
+```bash
+node counter.js "app:adressenavn" "Basisdata_0000_Norge_4258_MatrikkelenVegadresse_GML.zip" "Address Street Name Frequency in Norway" "Shows the amount of addresses per street name in Norway. Data from Kartverket."
+```
+Complete address street name frequencies for Norway.
--- counter.js
+++ counter.js
@@ -1,4 +1,3 @@
-const { generateKeyPair } = require('crypto');
 const fs = require('fs');
 const path = require("path");
 const json2html = require('node-json2html');
@@ -6,15 +5,6 @@ const json2html = require('node-json2html');
 const DEBUG = false;
 const pageTemplatePath = __dirname + "/template.html";
 const saveInterval = 1 * 60000; /* one minute in ms */
-
-let output = {
-	startTime: Date.now(),
-	lastSaveTime: Date.now(),
-	isFinished: false,
-	totalItemsChecked: 0,
-	uniqueItems: 0,
-	itemCounters: {}
-};
 
 const args = process.argv.slice(2);
 
@@ -27,6 +17,29 @@ if (DEBUG) { console.log("Input path: " + inputPath); }
 const inputFileName = path.basename(inputPath);
 const outputPath = __dirname + "/output/" + inputFileName;
 if (DEBUG) { console.log("Output path: " + outputPath); }
+
+let output = {
+	input: inputFileName,
+	title: null,
+	description: null,
+	tag: keyName,
+	startTime: Date.now(),
+	lastSaveTime: Date.now(),
+	isFinished: false,
+	totalTagsChecked: 0,
+	uniqueTagContents: 0,
+	counters: {}
+};
+
+if (typeof args[2] !== 'undefined') {
+	output.title = args[2];
+	if (DEBUG) { console.log("Title: " + inputPath); }
+}
+
+if (typeof args[3] !== 'undefined') {
+	output.description = args[3];
+	if (DEBUG) { console.log("Description: " + inputPath); }
+}
 
 function updateLine(line){
 	process.stdout.clearLine();
@@ -70,23 +83,88 @@ function formatBytes(a,b=2){if(0===a)return"0 Bytes";const c=0>b?0:b,d=Math.floo
 function saveOutputAsJson() {
 	let outputString = JSON.stringify(output);
 
-	console.log(" - Writing JSON file");
+	console.log(` - Writing JSON file to output/${inputFileName}.json`);
 
 	fs.writeFileSync(outputPath + ".json", outputString, 'utf8');
 
 	output.lastSaveTime = Date.now();
 }
 
-function writeWebPage(title, tableContents) {
+function saveOutputAsWebpage() {
 	fs.readFile(pageTemplatePath, 'utf8', function (error, page) {
 		if (error) {
-			console.error(error); return;
+			console.error(error);
+			return;
 		}
 
-		let pageWithTitle = page.replace(/{%!TITLE!%}/g, title);
-		let finishedPage = pageWithTitle.replace(/{%!TABLE-CONTENTS!%}/g, tableContents);
+		let counters = Object.entries(output.counters);
 
-		fs.writeFileSync(outputPath + ".html", finishedPage, 'utf8');
+		let sortedCounters = counters.sort((a, b) => b[1] - a[1]);
+	
+		let outputData = sortedCounters.slice(0, 100);
+	
+		let headerTemplate = {"<>":"thead","html":[
+			{"<>":"tr","html":[
+				{"<>":"td","text":"Name"},
+				{"<>":"td","text":"Count"}
+			]}
+		]};
+	
+		let contentTemplate = {
+			"row": {
+				"<>":"tr","html": [
+					{"<>":"td","text":"${0}"},
+					{"<>":"td","text":"${1}"}
+				]
+			},
+			"tableBody": {
+				"<>":"tbody",'html': function() {
+					return json2html.transform(outputData, contentTemplate.row);
+				}
+			}
+		};
+	
+		let tableContents = json2html.transform({}, headerTemplate) + json2html.transform({}, contentTemplate.tableBody);
+		
+		let endTime = Date.now();
+
+		let runTime = new Date(endTime - output.startTime).toISOString().substr(11, 8);
+		runTime = runTime.replace(":", "h ");
+		runTime = runTime.replace(":", "m ");
+		runTime += "s";
+		runTime = runTime.replace(/(^|\D)0+[hms ]/gm, ""); /* remove empty fields */
+		runTime = runTime.replace(/(^|[hms ])(0+)(?![hms ])/gm, "$1"); /* remove leading zeros */
+		runTime = runTime.trim();
+
+		let siteTitle = `Frequency of ${output.tag} Tag Content in ${output.input}`;
+		let tableTitle = `Frequency of <var>${output.tag}</var> Tag Content in <var>${output.input}</var>`;
+
+		if (output.title != null) {
+			siteTitle = output.title;
+			tableTitle = output.title;
+		}
+
+		page = page.replace(/{%!SITE-TITLE!%}/g, siteTitle);
+		page = page.replace(/{%!TABLE-TITLE!%}/g, tableTitle);
+		
+		page = page.replace(
+			/{%!DESCRIPTION!%}/g,
+			output.description != null ? `<details><summary>Description</summary><p>${output.description}</p></details>` : ""
+		);
+
+		page = page.replace(/{%!TABLE-CONTENTS!%}/g, tableContents);
+
+		page = page.replace(/{%!INPUT!%}/g, output.input);
+		page = page.replace(/{%!TAG!%}/g, output.tag);
+		page = page.replace(/{%!TAG-COUNT!%}/g, output.totalTagsChecked);
+		page = page.replace(/{%!UNIQUE-COUNT!%}/g, output.uniqueTagContents);
+		page = page.replace(/{%!START-TIME!%}/g, new Date(output.startTime).toISOString());
+		page = page.replace(/{%!END-TIME!%}/g, new Date(endTime).toISOString());
+		page = page.replace(/{%!RUNTIME!%}/g, runTime);
+
+		console.log(` - Writing HTML file to output/${inputFileName}.html`);
+
+		fs.writeFileSync(outputPath + ".html", page, 'utf8');
 	});
 }
 
@@ -103,25 +181,25 @@ saxStream.on("opentag", function (tag) {
 
 saxStream.on("text", function (text) {
 	if (insideTextTag) {
-		output.totalItemsChecked++;
+		output.totalTagsChecked++;
 
-		if (output.totalItemsChecked % 5000 === 0) {
+		if (output.totalTagsChecked % 5000 === 0) {
 			if (Date.now() > output.lastSaveTime + saveInterval) {
 				saveOutputAsJson();
 			}
 		}
 
-		if (output.itemCounters.hasOwnProperty(text)) {
-			output.itemCounters[text]++; 
+		if (Object.prototype.hasOwnProperty.call(output.counters, text)) {
+			output.counters[text]++; 
 		} else {
-			output.itemCounters[text] = 1;
+			output.counters[text] = 1;
 
-			output.uniqueItems++;
+			output.uniqueTagContents++;
 
 			if (DEBUG) { console.log("New name: " + text); }
 			
-			if (output.uniqueItems % 1000 === 0) {
-				updateLine("Unique names:" + output.uniqueItems + " in " + formatBytes(roughSizeOfObject(output.itemCounters))); 
+			if (output.uniqueTagContents % 1000 === 0) {
+				updateLine("Unique names:" + output.uniqueTagContents + " in " + formatBytes(roughSizeOfObject(output.counters))); 
 			}
 		}
 
@@ -134,45 +212,12 @@ saxStream.on("end", function () {
 
 	output.isFinished = true;
 
-	saveOutputAsJson(output);
+	saveOutputAsJson();
 
-	let itemCounters = Object.entries(output.itemCounters);
-
-	let data = itemCounters.sort((a, b) => b[1] - a[1]);
-
-	let outputData = data.slice(0, 100);
-
-	let headerTemplate = {"<>":"thead","html":[
-		{"<>":"tr","html":[
-			{"<>":"td","text":"Name"},
-			{"<>":"td","text":"Count"}
-		]}
-	]};
-
-	let contentTemplate = {
-		"row": {
-			"<>":"tr","html": [
-				{"<>":"td","text":"${0}"},
-				{"<>":"td","text":"${1}"}
-			]
-		},
-		"tableBody": {
-			"<>":"tbody",'html': function() {
-				return json2html.transform(outputData, contentTemplate.row);
-			}
-		}
-	};
-
-	console.log("Writing HTML file");
-
-	let tableContentHtml = json2html.transform({}, headerTemplate) + json2html.transform({}, contentTemplate.tableBody);
-
-	writeWebPage(inputFileName, tableContentHtml);
-
-	console.log('Results written to ' + outputPath);
+	saveOutputAsWebpage();
 });
 
-saxStream.on("error", function (e) {
+saxStream.on("error", function (error) {
 	console.error(error)
 
 	this._parser.error = null;
