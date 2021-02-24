e8903898ea16bc7746a86e37a527cd302794c552
Space <space@471.no>
2021-02-24 02:48:43 +0100

    Add output stats, speed up counting, NOR place name output, DataTables

--- counter.js
+++ counter.js
@@ -3,10 +3,18 @@ const fs = require('fs');
 const path = require("path");
 const json2html = require('node-json2html');
 
-const names = [];
 const DEBUG = false;
-
 const pageTemplatePath = __dirname + "/template.html";
+const saveInterval = 1 * 60000; /* one minute in ms */
+
+let output = {
+	startTime: Date.now(),
+	lastSaveTime: Date.now(),
+	isFinished: false,
+	totalItemsChecked: 0,
+	uniqueItems: 0,
+	itemCounters: {}
+};
 
 const args = process.argv.slice(2);
 
@@ -14,7 +22,7 @@ const keyName = args[0].toUpperCase();
 if (DEBUG) { console.log("Tag name: " + keyName); }
 
 const inputPath = args[1];
-if (DEBUG) { console.log("Input path: " + text); }
+if (DEBUG) { console.log("Input path: " + inputPath); }
 
 const inputFileName = path.basename(inputPath);
 const outputPath = __dirname + "/output/" + inputFileName;
@@ -26,14 +34,57 @@ function updateLine(line){
 	process.stdout.write(line);
 }
 
-function writeWebPage(title, contents) {
+function roughSizeOfObject(object) {
+	let objectList = [];
+	let stack = [object];
+	let bytes = 0;
+	
+	while (stack.length) {
+		let value = stack.pop();
+
+		if (typeof value === 'boolean') {
+			bytes += 4;
+		}
+		else if (typeof value === 'string') {
+			bytes += value.length * 2;
+		}
+		else if (typeof value === 'number') {
+			bytes += 8;
+		}
+		else if (
+			typeof value === 'object'
+			&& objectList.indexOf(value) === -1
+		) {
+			objectList.push(value);
+
+			for(var i in value) {
+				stack.push(value[i]);
+			}
+		}
+	}
+	return bytes;
+}
+
+function formatBytes(a,b=2){if(0===a)return"0 Bytes";const c=0>b?0:b,d=Math.floor(Math.log(a)/Math.log(1024));return parseFloat((a/Math.pow(1024,d)).toFixed(c))+" "+["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][d]}
+
+function saveOutputAsJson() {
+	let outputString = JSON.stringify(output);
+
+	console.log(" - Writing JSON file");
+
+	fs.writeFileSync(outputPath + ".json", outputString, 'utf8');
+
+	output.lastSaveTime = Date.now();
+}
+
+function writeWebPage(title, tableContents) {
 	fs.readFile(pageTemplatePath, 'utf8', function (error, page) {
 		if (error) {
 			console.error(error); return;
 		}
 
 		let pageWithTitle = page.replace(/{%!TITLE!%}/g, title);
-		let finishedPage = pageWithTitle.replace(/{%!MAIN-CONTENTS!%}/g, contents);
+		let finishedPage = pageWithTitle.replace(/{%!TABLE-CONTENTS!%}/g, tableContents);
 
 		fs.writeFileSync(outputPath + ".html", finishedPage, 'utf8');
 	});
@@ -52,14 +103,26 @@ saxStream.on("opentag", function (tag) {
 
 saxStream.on("text", function (text) {
 	if (insideTextTag) {
-		const nameIndex = names.findIndex(name => name.name === text);
+		output.totalItemsChecked++;
 
-		if (nameIndex !== -1) {
-			names[nameIndex].count++; 
+		if (output.totalItemsChecked % 5000 === 0) {
+			if (Date.now() > output.lastSaveTime + saveInterval) {
+				saveOutputAsJson();
+			}
+		}
+
+		if (output.itemCounters.hasOwnProperty(text)) {
+			output.itemCounters[text]++; 
 		} else {
+			output.itemCounters[text] = 1;
+
+			output.uniqueItems++;
+
 			if (DEBUG) { console.log("New name: " + text); }
-			updateLine("Unique names: " + names.length);
-			names.push({ name: text , count: 1 });
+			
+			if (output.uniqueItems % 1000 === 0) {
+				updateLine("Unique names:" + output.uniqueItems + " in " + formatBytes(roughSizeOfObject(output.itemCounters))); 
+			}
 		}
 
 		insideTextTag = false;
@@ -69,35 +132,42 @@ saxStream.on("text", function (text) {
 saxStream.on("end", function () {
 	console.log('\nParsing finished.');
 
-	let data = names.sort((a, b) => b.count - a.count);
+	output.isFinished = true;
 
-	let outputString = JSON.stringify(data);
-	fs.writeFileSync(outputPath + ".json", outputString, 'utf8');
+	saveOutputAsJson(output);
+
+	let itemCounters = Object.entries(output.itemCounters);
+
+	let data = itemCounters.sort((a, b) => b[1] - a[1]);
+
+	let outputData = data.slice(0, 100);
+
+	let headerTemplate = {"<>":"thead","html":[
+		{"<>":"tr","html":[
+			{"<>":"td","text":"Name"},
+			{"<>":"td","text":"Count"}
+		]}
+	]};
 
 	let contentTemplate = {
 		"row": {
 			"<>":"tr","html": [
-				{"<>":"td","text":"${name}"},
-				{"<>":"td","text":"${count}"}
+				{"<>":"td","text":"${0}"},
+				{"<>":"td","text":"${1}"}
 			]
 		},
-		"table": {
-			"<>":"table","html":[
-				{"<>":"thead","html":[
-					{"<>":"tr","html":[
-						{"<>":"td","text":"Name"},
-						{"<>":"td","text":"Count"}
-					]}
-				]},
-				{"<>":"tbody",'html': function(){
-					return json2html.transform(data, contentTemplate.row);    
-				}}
-			]
+		"tableBody": {
+			"<>":"tbody",'html': function() {
+				return json2html.transform(outputData, contentTemplate.row);
+			}
 		}
 	};
 
-	let outputHtml = json2html.transform({}, contentTemplate.table);
-	writeWebPage(inputFileName, outputHtml);
+	console.log("Writing HTML file");
+
+	let tableContentHtml = json2html.transform({}, headerTemplate) + json2html.transform({}, contentTemplate.tableBody);
+
+	writeWebPage(inputFileName, tableContentHtml);
 
 	console.log('Results written to ' + outputPath);
 });
